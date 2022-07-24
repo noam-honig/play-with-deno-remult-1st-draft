c31838da3fed0391b199485f359aa05603730c19
Noam Honig <noam.honig@gmail.com>
2022-07-24 13:29:39 +0300

    working

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,45 @@
-import { opine, json,Router } from "https://deno.land/x/opine@2.2.0/mod.ts";
-import { Entity, EntityBase, Fields, Remult } from 'remult';
+import { opine, json, Router } from "https://deno.land/x/opine@2.2.0/mod.ts";
+import { BackendMethod, Entity, EntityBase, Fields, Remult, Validators } from 'remult';
 import { remultMiddleware } from 'remult/remult-middleware';
 import { createPostgresConnection } from './postgres.ts';
-@Entity("tasks", { allowApiCrud: true })
-class Task extends EntityBase {
-    @Fields.autoIncrement()
-    id = 0;
-    @Fields.string()
+@Entity("tasks", {
+    allowApiCrud: true
+})
+export class Task {
+    @Fields.uuid()
+    id!: string;
+
+    @Fields.string({
+        validate: Validators.required
+    })
     title = '';
+
+    @Fields.boolean()
+    completed = false;
+    @BackendMethod({ allowed: false })
+    static testForbidden() {
+    }
 }
-
-
 const app = opine();
 const r = new Router();
-app.use(r);
-
-app.use(json());
 
 const api = remultMiddleware({
-    dataProvider:  () => {
-        return createPostgresConnection({
-            connectionString: "postgres://postgres:MASTERKEY@localhost/postgres"
-        })
-    },
+
     entities: [Task]
 });
 
-
+app.use(json());
 app.use(api);
-app.use((r,res)=>{});
 
-app.get("/", async function (req, res) {
-    const repo = remult.repo(Task);
-    await repo.insert([{ title: "abc" }])
-    res.json({ "name:": "noam", "count": await repo.count() });
+app.get('/api/test', async (req, res) => {
+    //@ts-ignore
+    const remult: Remult = await api.getRemult(req);
+    res.json({ result: await remult.repo(Task).count() })
 });
-app.post('/', async (req, res) => {
-    console.log(req.body);
-    res.send("ok");
-})
-
-// const api = remultExpress({
-//     entities: [Task],
-//     bodyParser:false
-// })
-// app.use((req, res, next) => {
-//     //@ts-ignore
-//     api(req,res,next);
-// });
 
 
-
-
+const port = 3006;
 app.listen(
-    3000,
-    () => console.log("server has started on http://localhost:3000 🚀"),
+    port,
+    () => console.log("opine " + port),
 );
-
-
-
-const remult =// new Remult(new InMemoryDataProvider());
-    new Remult();
-
